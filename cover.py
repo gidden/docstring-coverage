0d319a79851ba52f4cf8246689d2c8489030b253
DataGreed <datagreed@gmail.com>
2012-07-26 15:02:12 +0400

    * вынес парсинг опций за пределы функции * переименовал функцию из main в get_docstring_coverage и добавил к ней докстринг + теперь основная функция возвращает отчет в виде кортежа словарей для использования сторонними модулями

--- cover.py
+++ cover.py
@@ -33,7 +33,7 @@ The code doesn't work on classes or functions that are eval'd into existence.
 
 import compiler
 
-usage = '''python coverage.py <pythonsourcefile or directory>
+usage = '''python coverage.py [options] <pythonsourcefile or directory>
 
 Prints a rundown of the classes, functions, and methods in the given module 
 that have not been given a docstring. 
@@ -41,7 +41,7 @@ that have not been given a docstring.
 Shows statistics on docstring coverage.
 '''
 
-version = "0.2"
+__version__ = "0.2"
 
 class DocStringCoverageVisitor(compiler.visitor.ASTVisitor):
     
@@ -91,28 +91,60 @@ GRADES=(
     ("not documented at all", 2),
 )
 
-def main():
-    import sys, os
-    from optparse import OptionParser
-    
-    parser = OptionParser(usage = usage, version="%prog " + version)
-    parser.add_option("-v", "--verbose", dest="verbosity", default="2",metavar="LEVEL",
-                      help="verbose level <0-3>",type="choice", choices = ['0','1','2','3'])
-    parser.add_option("-m", "--nomagic",
-                      action="store_false", dest="magic", default=True,
-                      help="don't count docstrings for __magic__ methods")
+def get_docstring_coverage(filenames, count_magic=True, skip_empty_files = True, verbose_level=0):
+    '''
+    Runs through files in filenames list and seeks classes and methods
+    for missing docstrings. 
+    Returns a report in a dictionary. Can also print the report if 
+    verbose_level is set to 1-3
 
-    (options, args) = parser.parse_args()
-    
-    
+    @param filenames: a list of string containing filenames with
+                      absolute or relative paths
+    @param count_magic: a bool. If False, skips all __magic__
+                        methods and does not include them in the report
+    @param skip_emtpty_files: a bool, if True, skips all empty files,
+                              still includes them in the report, but
+                              doesn't require a module docstring for them
+    @param verbose_level: an int, possible vallues 0..3:
+                          0 - does not print anything
+                          1 - prints only total stats
+                          2 - prints stats for every file
+                          3 - prints missing docstrings for every file
+    @rtype: list
+    @returns: returns a list of the following format:
+             [
+                 {'<filename>': 
+                                {
+                                    'missing': ["<method_or_class_name","..."],                                    
+                                    'module_doc': <True or False>,  #has module docstring 
+                                    'missing_count': <missing_count>,
+                                    'needed_count': <needed_docstrings_count>,
+                                    'coverage': <percent_of_coverage>,                                
+                                },
+                    ...
+                  },
+                  
+                  #totals
+                  {
+                      'missing_count': <total_missing_count>,
+                      'needed_count': <total_needed_docstrings_count>,
+                      'coverage': <total_percent_of_coverage>,                                
+                  },
+                  
+             ]
+                  
+    '''
+    verbose_level = int(verbose_level)
     
     def log(text, level=1, append=False):
         u"""Prints to log depending on verbosity level"""
-        if int(options.verbosity)>= level:
+        if verbose_level>= level:
             if append: print text,
             else: print text
             
     def printDocstring(base, node):
+        
+        missing_list = []
         
         docs_needed = 1
         docs_covered = 0
@@ -120,16 +152,106 @@ def main():
         name, isDoc, childNodes = node
         if isDoc == False:
             print ' - No docstring for %s%s!' % (base, name)
+            missing_list.append(base+name)
         else:
             #можно выводить, что есть докстринг при максимальной вербозности
             docs_covered+=1
             
         for symbol in childNodes:
-            temp_docs_needed, temp_docs_covered = printDocstring('%s.' % name, symbol)
+            temp_docs_needed, temp_docs_covered, temp_missing_list = printDocstring('%s.' % name, symbol)
             docs_needed+=temp_docs_needed
             docs_covered+=temp_docs_covered
+            missing_list += temp_missing_list
             
-        return docs_needed, docs_covered
+        return docs_needed, docs_covered, missing_list
+    
+    
+    #handle docstrings
+    total_docs_needed = 0       #for statistics
+    total_docs_covered = 0      #
+    
+    result_dict = {}        #a dictionary with file statisics
+    
+    for filename in filenames:
+        
+        print "\nFile %s" % filename
+        
+        file_docs_needed = 1    #module docstring
+        file_docs_covered = 1  #we assume we have module docstring
+        file_missing_list = []  #list of all symbos missing docstrings in file
+        
+        module = DocStringCoverageVisitor(filename).getResult()
+        
+        if not module[0]:
+            print " - No module dostring!"
+            file_docs_covered-=1    
+        
+        #traverse through functions and classes    
+        for symbol in module[1]:
+            temp_docs_needed, temp_docs_covered, missing_list = printDocstring('', symbol)
+            
+            file_docs_needed+=temp_docs_needed
+            file_docs_covered+=temp_docs_covered
+            file_missing_list+=missing_list
+            
+        total_docs_needed+=file_docs_needed
+        total_docs_covered+=file_docs_covered    
+        
+        result_dict[filename] = { 
+                                'missing': file_missing_list,                                    
+                                'module_doc': bool(module[0]),
+                                'missing_count': file_docs_needed-file_docs_covered,
+                                'needed_count': file_docs_needed,
+                                'coverage': float(file_docs_covered)*100/float(file_docs_needed)
+                                }
+                                
+        print " Needed: %s; Exist: %s; Missing: %s; Coverage: %.1f%%" % (file_docs_needed, file_docs_covered,
+                                                                    result_dict[filename]['missing_count'],
+                                                                    result_dict[filename]['coverage'])
+    
+    total_result_dict = {
+                        'missing_count': total_docs_needed - total_docs_covered,                                
+                        'needed_count': total_docs_needed,
+                        'coverage': float(total_docs_covered)*100/float(total_docs_needed),
+    }
+    
+    postfix=""
+    if(not count_magic): postfix=" (all magic methods omited!)"    
+    
+    if len(filenames)>1:
+        print "\nOverall statistics for %s files%s:" % (len(filenames), postfix)
+    else:
+        print "\nOverall statistics%s:" % postfix
+        
+    print "Docstrings needed: %s;" % total_docs_needed,
+    print "Docstings exist: %s;" % total_docs_covered,
+    print "Docstings missing: %s" % (total_result_dict['missing_count'])
+    print "Total docstring coverage: %.1f%%; " % (total_result_dict['coverage']), 
+    
+    grade = ""
+    for grade, value in GRADES:
+        if value<= total_result_dict['coverage']:
+            grade = grade
+            break;
+            
+    print "Grade: %s" % grade
+    
+    return result_dict, total_result_dict
+
+if __name__ == '__main__':
+    
+    import sys, os
+    from optparse import OptionParser
+    
+    #creating options
+    parser = OptionParser(usage = usage, version="%prog " + __version__)
+    parser.add_option("-v", "--verbose", dest="verbosity", default="2",metavar="LEVEL",
+                      help="verbose level <0-3>",type="choice", choices = ['0','1','2','3'])
+    parser.add_option("-m", "--nomagic",
+                      action="store_false", dest="magic", default=True,
+                      help="don't count docstrings for __magic__ methods")
+    #parsing options
+    (options, args) = parser.parse_args()
     
     #handle invalid args
     if len(args) !=1:
@@ -158,58 +280,4 @@ def main():
     if(not options.magic):
         raise NotImplementedError("Omitting magic methods not supported yet")
     
-    #finally, handle docstrings
-    
-    total_docs_needed = 0       #for statistics
-    total_docs_covered = 0      #
-    
-    for filename in filenames:
-        
-        print "\nFile %s" % filename
-        
-        file_docs_needed = 1    #module docstring
-        file_docs_covered = 1  #we assume we have module docstring
-        
-        module = DocStringCoverageVisitor(filename).getResult()
-        
-        if not module[0]:
-            print " - No module dostring!"
-            file_docs_covered-=1    
-        
-        #traverse through functions and classes    
-        for symbol in module[1]:
-            temp_docs_needed, temp_docs_covered = printDocstring('', symbol)
-            
-            file_docs_needed+=temp_docs_needed
-            file_docs_covered+=temp_docs_covered
-        
-        total_docs_needed+=file_docs_needed
-        total_docs_covered+=file_docs_covered    
-        
-        print " Needed: %s; Exist: %s; Missing: %s; Coverage: %s%%" % (file_docs_needed, file_docs_covered,
-                                                                    file_docs_needed-file_docs_covered,
-                                                                    file_docs_covered*100/file_docs_needed)
-    
-    postfix=""
-    if(not options.magic): postfix=" (all magic methods omited!)"
-        
-    if len(filenames)>1:
-        print "\nOverall statistics for %s files%s:" % (len(filenames), postfix)
-    else:
-        print "\nOverall statistics%s:" % postfix
-    print "Docstrings needed: %s;" % total_docs_needed,
-    print "Docstings exist: %s;" % total_docs_covered,
-    print "Docstings missing: %s" % (total_docs_needed - total_docs_covered)
-    percentage = total_docs_covered*100/total_docs_needed
-    print "Total docstring coverage: %s%%; " % (percentage), 
-    
-    grade = ""
-    for grade, value in GRADES:
-        if value<= percentage:
-            grade = grade
-            break;
-            
-    print "Grade: %s" % grade
-
-if __name__ == '__main__':
-    main()
+    get_docstring_coverage(filenames, options.magic, verbose_level = options.verbosity)
